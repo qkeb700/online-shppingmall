370113f9262e46da0a511675adccbdbcd8b7326a
Hoyeon Jang <46728564+qkeb700@users.noreply.github.com>
2021-10-10 17:45:53 +0900

    work on weekly best
    added linked buttons

--- js/script.js
+++ js/script.js
@@ -13,9 +13,40 @@ $(function(){
 
   //메인이미지 변환
   setInterval('fadeout()', 3000);
+
+  // weekly best에서 클릭했을 때 해당 리스트로 이동
+  $('.wb-list a').click(function(e){
+    e.preventDefault();
+    let link = $(this).data('link');
+    let lft;
+    switch(link){
+      case 'best':
+        lft = '0px';
+      break;
+      case 'top':
+        lft = '-1110px';
+      break;
+      case 'outer':
+        lft = '-2220px';
+      break;
+      case 'bottom':
+        lft = '-3330px';
+      break;
+      case 'dress':
+        lft = '-4440px';
+      break;
+    }
+    $('.wb-list a').removeClass('active');
+    $(this).addClass('active');
+    $('.container-in').animate({
+      left: lft
+    }, 500);
+  });
 });
 
 function fadeout(){
   $('.slider div:eq(0)').removeClass('z-index-2 z-index-1').clone().appendTo('.slider');
   $('.slider div:eq(0)').remove();
 }
+
+
