37161167b856d3c6df5150ba8f3c63ff28bb20df
Hoyeon Jang <46728564+qkeb700@users.noreply.github.com>
2021-10-18 01:06:21 +0900

    added json for color option
    get colors from json and output the colors on view.html

--- js/view.js
+++ js/view.js
@@ -6,5 +6,28 @@ $(function(){
         $('.smallImgs').find('img').removeClass('borderActive');
         $(this).addClass('borderActive');
         $('.bigImgBox').find('img').attr('src', img);
+    });
+
+    // json에서 컬러박스 색 가져오기
+    $.ajax({
+        url: "json/option.json",
+        dataType: "json",
+
+        success: function(data){
+            let colorOptions = data.color;
+            let radioInput = '';
+            let check = "";
+            let i = 0;    
+            for(const key in colorOptions){
+                if(i == 0){
+                    check = "checked";
+                }
+                radioInput += '<input type="radio" name="color" class="color"'+
+                              'id="' + key+'" data-opt1="'+colorOptions[key]+'" value="'+key+'" '+check+'>'+
+                              '<label for="'+key+'"></label>';
+                i++;
+            }
+            $('.colorbox').html(radioInput);
+        }
     })
 })
